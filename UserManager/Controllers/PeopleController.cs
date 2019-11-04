e4939bc0a5d70a4d5f557753de34bb6ef15d3626
Austin <augustineumeagudosi@gmail.com>
2019-11-04 03:14:27 +0100

    Add project files.

---
+++ UserManager/Controllers/PeopleController.cs
@@ -0,0 +1,46 @@
+﻿using UserManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserManager.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //get a list of people from the db in a json format
+        public ActionResult GetPeople()
+        {
+            var people = _db.Peoples.ToList();
+            return Json(new { data = people }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetPeoplePartial()
+        {
+            var people = new People();
+            return PartialView("_CreateOrUpdatePeoplePartial", people);
+        }
+
+        //save changes to db
+        public ActionResult CreateOrUpdatePeople(People people)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Peoples.Add(people);
+                _db.SaveChanges();
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ UserManager/Models/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManager.Models
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Height { get; set; }
+        public string HairColor { get; set; }
+        public string Weight { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+    }
+}
